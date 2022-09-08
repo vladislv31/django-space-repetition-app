fddbb2b0784b1205ad500777c2f2c6c01768bdf1
vladislv31 <vladislv.k31@gmail.com>
2022-09-09 02:25:08 +0300

    adding new cards and cats

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from .models import Category, Card
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Username')
@@ -7,3 +9,29 @@ class LoginForm(forms.Form):
 
     def invalid_login_error(self):
         self.add_error(None, forms.ValidationError('User with such login and password not found.'))
+
+
+class AddCategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name', 'parent']
+
+    def __init__(self, user, *args, **kwargs):
+        super(AddCategoryForm, self).__init__(*args, **kwargs)
+        self.fields['parent'].queryset = Category.objects.filter(user=user)
+
+
+class AddCardForm(forms.ModelForm):
+    class Meta:
+        model = Card
+        fields = ['question', 'answer', 'category']
+
+    def __init__(self, user, *args, **kwargs):
+        super(AddCardForm, self).__init__(*args, **kwargs)
+        self.fields['category'].queryset = Category.objects.filter(user=user)
+
+    def clean_category(self):
+        category = self.cleaned_data['category']
+        if not category:
+            raise forms.ValidationError('Category is required field!')
+        return category
--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.dashboard_view, name='dashboard'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
     path('learn/<int:category_id>', views.learn_card_view, name='learn'),
+    path('new-card/', views.add_new_card_view, name='add_new_card'),
+    path('new-category/', views.add_new_category_view, name='add_new_category'),
+    path('delete-category/<int:category_id>', views.delete_category_view, name='delete_category'),
 ]
